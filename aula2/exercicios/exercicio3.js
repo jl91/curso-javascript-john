25fbb7d011c73426507896e047674f6238471ccf
John Lennon de Melo Fernandes <jlfernandes91@hotmail.com>
2023-08-03 22:03:14 -0300

    adicionando mais um exemplo de utilização de arrays com laço for

--- aula2/exercicios/exercicio3.js
+++ aula2/exercicios/exercicio3.js
@@ -2,3 +2,9 @@
  * Verificador de Palíndromo: Você já ouviu falar de palavras que são iguais de trás para frente? 
  * São chamadas de palíndromos. Escreva um programa que descubra se uma palavra que você digitar é um palíndromo.
  */
+
+
+const name = "john";
+
+
+console.log(name.charAt(0));// j
--- aula2/exercicios/exercicio9.js
+++ aula2/exercicios/exercicio9.js
@@ -1,4 +1,5 @@
 /**
  * Validação de Senha: Construa um guarda de segurança digital! 
  * Crie um programa que verifica se a senha que você escolheu é forte o suficiente para proteger suas coisas.
+ * uma senha forte o suficiente deve ter ao menos 8 digitos, conter números e letras maíusculas e minúsculas
  */
