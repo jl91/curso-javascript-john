be4f36c4cfa840988eb3942228bb4530d4ec79f9
John Lennon de Melo Fernandes <jlfernandes91@hotmail.com>
2023-08-25 00:35:19 -0300

    corrigindo ordenação por palavras

--- aula2/exercicios/exercicio11.js
+++ aula2/exercicios/exercicio11.js
@@ -67,16 +67,47 @@ for (let index = 0; index < words.length; index++) {
 
     const actualWord = words[index];
 
-    const actualWordLetter = actualWord.charAt(0);
-    const nextWordLetter = nextWord.charAt(0);
 
-    const alphabetIndexActualLetter = alphabetMap[actualWordLetter];
-    const alphabetIndexNextLetter = alphabetMap[nextWordLetter];
+    for (let index2 = 0; index2 < actualWord.length; index2++) {
 
-    if (alphabetIndexActualLetter > alphabetIndexNextLetter) {
-        words[index] = nextWord;
-        words[index + 1] = actualWord;
-        index = -1;
+        const actualWordActualLetter = actualWord.charAt(index2);
+        let actualWordPreviousLetter = 0;
+
+        const nextWordActualLetter = nextWord.charAt(index2);
+        let nextWordPreviousLetter = 0;
+
+        const actualLetterIndexInAlphabet = alphabetMap[actualWordActualLetter];
+        let nextLetterIndexInAlphabet = alphabetMap[nextWordActualLetter];
+
+        let actualWordPreviousLetterIndexInAlphabet = 0;
+        let nextWordPreviousLetterIndexInAlphabet = 0;
+
+        if (index2 > 0) {
+            actualWordPreviousLetter = actualWord.charAt(index2 - 1);
+            nextWordPreviousLetter = nextWord.charAt(index2 - 1);
+
+            actualWordPreviousLetterIndexInAlphabet = alphabetMap[actualWordPreviousLetter];
+            nextWordPreviousLetterIndexInAlphabet = alphabetMap[nextWordPreviousLetter];
+
+            if (
+                actualLetterIndexInAlphabet > nextLetterIndexInAlphabet
+                && actualWordPreviousLetterIndexInAlphabet === nextWordPreviousLetterIndexInAlphabet
+            ) {
+                words[index] = nextWord;
+                words[index + 1] = actualWord;
+                index = -1;
+                break;
+            }
+
+
+        } else if (actualLetterIndexInAlphabet > nextLetterIndexInAlphabet) {
+            words[index] = nextWord;
+            words[index + 1] = actualWord;
+            index = -1;
+            break;
+        }
+
+
     }
 }
 
